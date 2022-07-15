c14e0263b37a6b72f00ee1d92bfc9c236e196722
David Niño Herrán <45274593+davidnherran@users.noreply.github.com>
2022-07-14 20:12:26 -0500

    core: create an structure

---
+++ src/App.jsx
@@ -0,0 +1,24 @@
+import React from 'react';
+import { Container } from '@mui/material';
+import Searcher from './components/searcher';
+
+const App = () => {
+  return (
+    <Container
+      sx={{
+        background: 'whitesmoke',
+        width: '80vw',
+        height: '500px',
+        borderRadius: '16px',
+        marginTop: '40px',
+        display: 'flex',
+        flexDirection: 'column',
+        alignItems: 'center',
+      }}
+    >
+      <Searcher />
+    </Container>
+  );
+};
+
+export default App;
